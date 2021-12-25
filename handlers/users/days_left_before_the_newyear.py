bbe543255967619d254b9586253aaf841d01e7e7
Abrik <abrormaksudov2207@gmail.com>
2021-12-25 19:52:25 +0500

    minor changes

--- handlers/users/days_left_before_the_newyear.py
+++ handlers/users/days_left_before_the_newyear.py
@@ -2,28 +2,26 @@ from datetime import datetime, date
 
 from aiogram import types
 from aiogram.dispatcher.filters import Command
+from aiogram.utils import emoji
 
 from loader import dp
 
 
 @dp.message_handler(Command("newyear"))
 async def days_left_before_new_year(message: types.Message):
-    NEWYEAR = datetime(year=date.today().year, month=1, day=1)
+    NEWYEAR = datetime(year=date.today().year, month=12, day=31, hour=23,
+                       minute=59, second=59, microsecond=999999)
     today = datetime.today()
 
-    if today > NEWYEAR:
-        NEWYEAR = NEWYEAR.replace(year=date.today().year + 1)
-
+    if NEWYEAR > today:
         days_left = NEWYEAR - today
-        hours_left = today.hour - NEWYEAR.hour
-        minutes_left = today.minute - NEWYEAR.minute
-        await message.answer(f"До Нового Года осталось {days_left.days} дней, "
-                             f"{24 - hours_left} "
-                             f"часов и {60 - minutes_left} минуты!")
+        hours_left = NEWYEAR.hour - datetime.now().hour
+        minutes_left = NEWYEAR.minute - today.minute
+        await message.answer(f"До Нового Года осталось {days_left.days} дней, {hours_left} "
+                             f"часов и {minutes_left} минут!{emoji.emojize(':snowman:')}")
     else:
-        days_left = NEWYEAR - today
-        hours_left = today.hour - NEWYEAR.hour
-        minutes_left = today.minute - NEWYEAR.minute
-        await message.answer(f"До Нового Года осталось {days_left.days} дней, "
-                             f"{24 - hours_left} часов и "
-                             f"{60 - minutes_left} минуты!")
+        days_left = today - NEWYEAR
+        hours_left = NEWYEAR.hour - datetime.now().hour
+        minutes_left = NEWYEAR.minute - today.minute
+        await message.answer(f"До Нового Года осталось {days_left.days} дней, {hours_left} "
+                             f"часов и {minutes_left} минут!{emoji.emojize(':snowman:')}")
