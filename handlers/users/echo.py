dafc6dbf1edec2e421c3ed21df0bc7b5215d0c47
Abrik <abrormaksudov2207@gmail.com>
2021-12-25 17:56:24 +0500

    minor changes

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -7,13 +7,14 @@ from loader import dp
 # Эхо хендлер, куда летят текстовые сообщения без указанного состояния
 @dp.message_handler(state=None)
 async def bot_echo(message: types.Message):
-    await message.answer(f"")
+    await message.answer(f"Упс... Вы ввели что-то не понятное.\n\n"
+                         f"Подробная информация о боте по команде /help")
 
 
 # Эхо хендлер, куда летят ВСЕ сообщения с указанным состоянием
 @dp.message_handler(state="*", content_types=types.ContentTypes.ANY)
 async def bot_echo_all(message: types.Message, state: FSMContext):
     state = await state.get_state()
-    await message.answer(f"Эхо в состоянии <code>{state}</code>.\n"
-                         f"\nСодержание сообщения:\n"
-                         f"<code>{message}</code>")
+    await message.answer(f"Упс... Вы ввели что-то не понятное.\n\n"
+                         f"Подробная информация о боте по команде /help\n"
+                         f"Вы в состоянии <code>{state}</code>.")
