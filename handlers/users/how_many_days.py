25b422c4456f1bfab6b970014372410cbca01c61
Abrik <abrormaksudov2207@gmail.com>
2021-12-28 09:46:02 +0500

    added function how_many_days.py

--- handlers/users/how_many_days.py
+++ handlers/users/how_many_days.py
@@ -1,10 +1,36 @@
+from datetime import datetime
+
 from aiogram import types
+from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Command
 
 from loader import dp
+from states.how_many_days_state import HMD
 
 
 @dp.message_handler(Command("howmanydays"))
 async def days_left(message: types.Message):
-    await message.answer("Эта команда находится в разработке. "
-                         "Приносим прощения за временные не удобства.")
+    await message.answer("Отправьте дату, чтобы узнать разницу между текущей датой "
+                         "и датой отправленной вами (например 01.01.2077):")
+
+    await HMD.DATE.set()
+
+
+@dp.message_handler(state=HMD.DATE)
+async def days_left_before_the_date(message: types.Message, state: FSMContext):
+    user_date = message.text
+    try:
+        parsed_date = datetime.strptime(user_date, "%d.%m.%Y")
+        today = datetime.today()
+
+        if parsed_date > today:
+            difference = parsed_date - today
+            await message.answer(f"До {user_date} осталось {difference.days} дней")
+        elif today > parsed_date:
+            difference = today - parsed_date
+            await message.answer(f"С {user_date} прошло {difference.days} дней")
+    except ValueError as ex:
+        await message.answer("Вы некорректно ввели дату.")
+        await state.reset_state()
+
+    await state.reset_state()
--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -7,10 +7,9 @@ async def set_default_commands(dp):
     usercommands = [
         BotCommand(command="start", description="Запустить бота"),
         BotCommand(command="help", description="Помощь"),
-        BotCommand(command="mybd", description="Сколько дней до дня рождения"),
-        BotCommand(command="newyear", description="Сколько дней до Нового Года"),
-        BotCommand(command="howmanydays", description="Подсчитать количество дней с "
-                                                      "и до определенного дня")
+        BotCommand(command="mybd", description="Сколько дней осталось до дня рождения"),
+        BotCommand(command="newyear", description="Сколько дней осталось до Нового Года"),
+        BotCommand(command="howmanydays", description="Сколько дней осталось до даты")
     ]
     await bot.set_my_commands(usercommands, scope=BotCommandScopeDefault())
 
@@ -19,8 +18,7 @@ async def set_default_commands(dp):
         BotCommand(command="help", description="Помощь"),
         BotCommand(command="mybd", description="Сколько дней до дня рождения"),
         BotCommand(command="newyear", description="Сколько дней до Нового Года"),
-        BotCommand(command="howmanydays", description="Подсчитать количество дней с "
-                                                      "и до определенного дня"),
+        BotCommand(command="howmanydays", description="Сколько дней осталось до даты"),
         BotCommand(command="statistics", description="Статистику бота"),
         BotCommand(command="show_state", description="Показать текущее состояние")
     ]
