287bf94eaef72076fd78ef92a6485b1df4a1a7dd
Abrik <abrormaksudov2207@gmail.com>
2022-01-12 14:22:50 +0500

    minor changes

--- handlers/users/botinfo.py
+++ handlers/users/botinfo.py
@@ -11,4 +11,4 @@ async def bot_info(message: types.Message):
                                 f"Beta version {hcode('0.0.1')}\n"
                                 "Updated on Jan 9, 2022\n"
                                 "Released on Dec 25, 2021\n"
-                                "Developer @JustAbrik")
+                                "Developer @JustAbrik", disable_web_page_preview=True)
