262e7e42e59fda9242a6afa554f306cebddc172b
abrikk <abrormaksudov2207@gmail.com>
2022-04-17 11:35:24 +0500

    update

--- tgbot/misc/notify_admins.py
+++ tgbot/misc/notify_admins.py
@@ -8,7 +8,7 @@ async def on_startup_notify(bot: Bot):
     config = bot.get('config')
     for admin in config.tg_bot.admin_ids:
         try:
-            await bot.send_message(admin, "Бот Запущен")
+            await bot.send_message(admin, "Бот Запущен на ветке для разработки")
         except BotBlocked:
             logging.warning("Forbidden: bot was blocked by the user")
         except Exception as err:
