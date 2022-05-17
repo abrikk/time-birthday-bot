9fbfd19ed41bb05e6124dced38117894e7d872e1
abrikk <abrormaksudov2207@gmail.com>
2022-05-17 20:53:28 +0500

    learning filters

--- tgbot/keyboards/inline.py
+++ tgbot/keyboards/inline.py
@@ -2,7 +2,9 @@ from datetime import date
 
 from aiogram import types, Dispatcher
 from aiogram.dispatcher.filters import Text
+from aiogram.types import ChatType
 
+from tgbot.filters.private_chat import ChatTypeFilter
 from tgbot.functions.birthday_func import birthday_btn
 from tgbot.functions.gettext_func import get_newyear_time, until_bd
 from tgbot.keyboards.reply import switch_to_bot, switch_or_gratz
@@ -10,6 +12,7 @@ from tgbot.middlewares.lang_middleware import _, __
 
 
 async def all_queries(query: types.InlineQuery, db_commands):
+    print(query.chat_type)
     user = await db_commands.get_user(user_id=query.from_user.id)
     query_offset = int(query.offset) if query.offset else 0
     holidays = await db_commands.get_holidays(lang=user.lang_code, like=query.query,
@@ -30,33 +33,6 @@ async def all_queries(query: types.InlineQuery, db_commands):
     else:
         await query.answer(results, cache_time=3, is_personal=True,
                            next_offset=str(query_offset + 50))
-    # else:
-    #     days_left, age = await birthday_btn(user)
-    #     await query.answer(
-    #         results=[
-    #             types.InlineQueryResultArticle(
-    #                 id="newyear",
-    #                 title=_("Количество дней оставшееся до Нового Года."),
-    #                 description=_("Нажмите чтобы отправить сколько дней осталось до Нового Года в текущий чат."),
-    #                 input_message_content=types.InputTextMessageContent(
-    #                     message_text=get_newyear_time()
-    #                 ),
-    #                 reply_markup=switch_to_bot()
-    #             ),
-    #             types.InlineQueryResultArticle(
-    #                 id="share",
-    #                 title=await until_bd(days_left, age, where="title", user_bd=user.user_bd),
-    #                 description=_(
-    #                     "Нажмите чтобы отправить сколько дней осталось до вашего дня рождения в текущий чат."),
-    #                 input_message_content=types.InputTextMessageContent(
-    #                     message_text=await until_bd(days_left, age, "inline_text", user_bd=user.user_bd)
-    #                 ),
-    #                 reply_markup=switch_to_bot() if days_left != 0 else switch_or_gratz(query.from_user.id)
-    #             )
-    #         ],
-    #         cache_time=5,
-    #         is_personal=True
-    #     )
 
 
 async def newyear_query(query: types.InlineQuery):
@@ -79,7 +55,6 @@ async def newyear_query(query: types.InlineQuery):
 async def bd_query(query: types.InlineQuery, db_commands):
     user = await db_commands.get_user(user_id=query.from_user.id)
     days_left, age = await birthday_btn(user)
-
     await query.answer(
         results=[
             types.InlineQueryResultArticle(
@@ -100,4 +75,4 @@ async def bd_query(query: types.InlineQuery, db_commands):
 def register_inline_mode(dp: Dispatcher):
     dp.register_inline_handler(bd_query, Text(contains=__("my birthday"), ignore_case=True))
     dp.register_inline_handler(newyear_query, Text(contains=__("new year"), ignore_case=True))
-    dp.register_inline_handler(all_queries)
+    dp.register_inline_handler(all_queries, ChatTypeFilter(ChatType.SENDER))
